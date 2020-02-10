fbec004e891604d2c06a91f39ef029bfbcbdbad5
KID32 <60864213+KID32@users.noreply.github.com>
2020-02-10 22:54:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ var config = {
         "啧啧啧",
         "时光真是飞快",
         "来迟的我",
-        "祝你生日快乐！",
+        "祝你生日快乐~",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -29,13 +29,13 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "点击一下就开始",
-        play: "音乐,会有声音哦",
+        turn_on: "点击一下就开始了~",
+        play: "音乐,会有声音哦~",
         bannar_coming: "颜色的灯光",
-        balloons_flying: "好像少点东西",
+        balloons_flying: "好像少点东西？",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        wish_message: "生日快乐~",
         story: "A MESSAGE FOR YOU",
     }
 };
